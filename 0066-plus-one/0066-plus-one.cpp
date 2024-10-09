129e5b7c4a8dd874277c5068db454ebe7f4b9667
Sudhanshu Shivam <157030076+shiv-xm@users.noreply.github.com>
2024-10-09 10:51:35 +0530

    Time: 5 ms (26.84%), Space: 10.4 MB (81.14%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -6,10 +6,10 @@ public:
         if (digits[i] < 9) {
             digits[i]++; 
             return digits;
-        }
-        digits[i] = 0;
+        }else{
+            digits[i] = 0; 
+        } 
     }
-    
     digits.insert(digits.begin(), 1);
     return digits;
 } 
