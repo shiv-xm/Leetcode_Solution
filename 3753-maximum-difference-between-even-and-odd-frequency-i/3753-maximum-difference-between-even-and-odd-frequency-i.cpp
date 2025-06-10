0e65e298a3e9ea114d12ab802c4a5c6c881d966e
Sudhanshu Shivam <157030076+shiv-xm@users.noreply.github.com>
2025-06-10 09:12:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char, int> freq;
+        
+        for (char c : s) {
+            freq[c]++;
+        }
+        
+        int maxOdd = INT_MIN;
+        int minEven = INT_MAX;
+        
+        for (auto &entry : freq) {
+            if (entry.second % 2 != 0) {
+                maxOdd = max(maxOdd, entry.second);
+            } else {
+                minEven = min(minEven, entry.second);
+            }
+        }
+
+        if (maxOdd == INT_MIN || minEven == INT_MAX) 
+            return 0;
+        }
+
+        return maxOdd - minEven;
+    }
+};
